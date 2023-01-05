7f2258d909e0df5cf3d7a9a8727c055b758ebf3b
Arpit Patel <arpitp@siloho.com>
2023-01-06 03:12:12 +0530

    Ingestor: Created YTVideo Model and Admin Interface

--- server/Ingestor/admin.py
+++ server/Ingestor/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from Ingestor.models import *
 # Register your models here.
+
+@admin.register(YoutubeVideo)
+class YoutubeVideoAdmin(admin.ModelAdmin):
+    list_display = [
+        'video_id',
+        'title',
+        'description',
+        'channel_id',
+        'channel_title',
+        'published_at',
+        'thumbnail',
+        'raw_data'
+    ]
+    
+    search_fields = [
+        'id',
+        'video_id',
+        'title'
+    ]
+    
+    list_filter = [
+        'channel_title',
+        'channel_id'
+    ]
+    
+    
--- server/Ingestor/models.py
+++ server/Ingestor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class YoutubeVideo(models.Model):
+    video_id = models.CharField(max_length=50, unique=True)
+    published_at = models.DateTimeField()
+    channel_id = models.CharField(max_length=75)
+    title = models.TextField()
+    description = models.TextField()
+    thumbnail = models.URLField(null=True)
+    channel_title = models.CharField(max_length=250, null=True)
+    raw_data = models.JSONField(null=True)
+    
+    def __str__(self):
+        return f'{self.video_id} -> {self.title}'
